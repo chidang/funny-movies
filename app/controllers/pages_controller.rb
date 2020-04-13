54e040e5270450127b89e4b975c5ca11b007eab3
Dang Chi <chid@jobready.com.au>
2020-04-13 16:55:13 +0700

    FM-7: Limitting Access if User not yet logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authorize
+
   def index
     @movies = Movie.all
   end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,42 +1,73 @@
 require 'rails_helper'
 
 describe MoviesController, type: :controller do
+  let!(:user) { create(:user)}
+
   describe 'GET #new' do
-    before { get :new }
-
-    specify do
-      expect(response.response_code).to eq 200
-      expect(response).to render_template('movies/new')
-    end
-  end
-
-  describe 'POST #create' do
-    let!(:user) { create(:user) }
-
-    let(:movie_params) { { youtube_url: 'http://www.youtube.com/watch?v=123456'} }
-
-    context 'when create movie is successful' do
-      before do
-        allow_any_instance_of(MovieCreator).to receive(:perform)
-          .and_return([true, {success: 'Movie was successfully created.'}])
-        post :create, params: { movie: movie_params }
-      end
+    context 'when user not yet logged in' do
+      before { get :new }
 
       specify do
+        expect(response.response_code).to eq 302
         expect(response).to redirect_to root_path
       end
     end
 
-    context 'when create movie is unsuccessful' do
+    context 'when user logged in' do
       before do
-        allow_any_instance_of(MovieCreator).to receive(:perform)
-          .and_return([false, {danger: 'Video is not available'}])
-        post :create, params: { movie: movie_params }
+        controller.session[:user_id] = user.id
+        controller.session[:user_email] = user.email
+        get :new 
       end
 
       specify do
         expect(response.response_code).to eq 200
         expect(response).to render_template('movies/new')
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    let(:movie_params) { { youtube_url: 'http://www.youtube.com/watch?v=123456'} }
+
+    context 'when user not yet logged in' do
+      before { post :create, params: { movie: movie_params } }
+
+      specify do
+        expect(response.response_code).to eq 302
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'when user logged in' do
+      before do
+        controller.session[:user_id] = user.id
+        controller.session[:user_email] = user.email
+      end
+
+      context 'and create movie is successful' do
+        before do
+          allow_any_instance_of(MovieCreator).to receive(:perform)
+            .and_return([true, {success: 'Movie was successfully created.'}])
+          post :create, params: { movie: movie_params }
+        end
+
+        specify do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'and create movie is unsuccessful' do
+        before do
+          allow_any_instance_of(MovieCreator).to receive(:perform)
+            .and_return([false, {danger: 'Video is not available'}])
+          post :create, params: { movie: movie_params }
+        end
+
+        specify do
+          expect(response.response_code).to eq 200
+          expect(response).to render_template('movies/new')
+        end
       end
     end
   end
