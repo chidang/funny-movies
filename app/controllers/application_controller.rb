d6a5e91edd152ceea26ce6ccd555e003ca3aede9
Chi Dang <chid@jobready.com.au>
2020-04-18 16:11:14 +0700

    FM-15: Fix raise ActionController::InvalidAuthenticityToken (#16)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true, with: :exception
   before_action :authorize
+  skip_before_filter :verify_authenticity_token
 
   private
 
